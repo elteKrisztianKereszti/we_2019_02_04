b995de4a3dd20613fc2d1206ed92b457161bb7ef
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2020-03-06 16:08:39 +0100

    init Maven project

---
+++ src/test/java/hu/elte/IssueTracker/IssueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.elte.IssueTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
