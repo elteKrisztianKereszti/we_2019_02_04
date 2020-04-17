194c111669a047a55fec3fea04eb931868133dba
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2020-04-17 13:39:06 +0200

    Add registration function

--- src/main/java/hu/elte/IssueTracker/controllers/AuthController.java
+++ src/main/java/hu/elte/IssueTracker/controllers/AuthController.java
@@ -5,20 +5,50 @@
  */
 package hu.elte.IssueTracker.controllers;
 
+import hu.elte.IssueTracker.entities.User;
+import hu.elte.IssueTracker.repositories.UserRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
  * @author KeresztiKrisztián
  */
-
 @Controller
 public class AuthController {
+
+  @Autowired
+  private BCryptPasswordEncoder passwordEncoder;
+
+  @Autowired
+  private UserRepository userRepository;
 
   @GetMapping("/login")
   public String login() {
     return "login";
   }
-  
+
+  @GetMapping("/register")
+  public String registerForm() {
+    return "register";
+  }
+
+  @PostMapping("/register")
+  public String register(User user, Model model) {
+    Optional<User> oUser = userRepository.findByUsername(user.getUsername());
+    if (oUser.isPresent()) {
+      model.addAttribute("error", "Existing user name");
+      return "register";
+    }
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
+    user.setEnabled(true);
+    user.setRole(User.Role.ROLE_USER);
+    userRepository.save(user);
+    return "redirect:/login";
+  }
 }
--- src/main/java/hu/elte/IssueTracker/security/WebSecurityConfig.java
+++ src/main/java/hu/elte/IssueTracker/security/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http
       .authorizeRequests()
-        .antMatchers("/", "/h2/**").permitAll()
+        .antMatchers("/", "/h2/**", "/register").permitAll()
         .anyRequest().authenticated()
         .and()
       .formLogin()
