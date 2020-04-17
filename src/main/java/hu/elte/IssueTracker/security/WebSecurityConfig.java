64a6b2cc3c20f05f12cb97f3aa3366fdb11350d3
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2020-04-17 12:38:26 +0200

    Security config: Add memory user, protect all endpoint (expect '/')

--- src/main/java/hu/elte/IssueTracker/security/WebSecurityConfig.java
+++ src/main/java/hu/elte/IssueTracker/security/WebSecurityConfig.java
@@ -21,18 +21,29 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
 
-    }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .authorizeRequests()
+        .antMatchers("/").permitAll()
+        .anyRequest().authenticated()
+        .and()
+      .formLogin()
+        .and()
+      .httpBasic();
+  }
 
-    @Autowired
-    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+  @Autowired
+  protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    // user/password
+    auth
+      .inMemoryAuthentication()
+      .withUser("user").password("$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..").roles("USER");
+  }
 
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
